380ef4a56a78c1c62e18677a631fc247a0be96fc
yoo gayeong <asom8650@gmail.com>
2025-06-04 14:23:51 +0900

    first commit

--- app/guestbook/[id]/page.tsx
+++ app/guestbook/[id]/page.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
-import { Guestbook } from "@/app/type/type";
+import { Guestbook } from "../../type/type";
+
 
 
 
